4f7d56d9022fd18b070ab6e1fa15b5ec22ef1299
philiflo <delrosapf@gmail.com>
2024-11-07 07:48:26 +0000

    made script for symbolic links, doesn't work yet

--- install_packages.sh
+++ install_packages.sh
@@ -1,34 +1,4 @@
 #!/bin/bash
-#shebang ^
-
-# Define packages to be installed
-#packages=("kakoune", "tmux")
-
-# Check if pacman is installed on the device, if so, install the packages stored in the packages list
-#for pack in "${packages[@]}"; do
-
-# Check if the package is already installed
-#	if pacman -Qi "$pack" &>/dev/null; then
-#		echo "$pack is already installed."
-#	else
-#		echo "Installing $pack.."
-
-# If it isn't, start installation using -S for that specific package
-#	if pacman -S --noconfirm  "$pack"; then
-#		echo "$pack installed successfully."
-#	else 
-#		echo "Installing $pack failed" >&2
-#		exit 1
-#	fi
-#fi
-#done 
-
-# Tells user that packages have been installed
-#echo "Packages installed!"
-
-
-#!/bin/bash
-
 
 # Define a list of packages to install
 packages=("kakoune" "tmux")  # Remove any commas in the list
--- link_config.sh
+++ link_config.sh
@@ -6,6 +6,9 @@ REPO_DIR="/home/arch/.git/2420_assignment_2"
 
 # Symbolic links for bin and config
 
+
 ln -snf "$REPO_DIR/bin" ~/bin
-ln -snf "$REPO_DIR/config" ~/config 
+ln -snf "$REPO_DIR/config" ~/config
+
+ln -snf "$REPO_DIR/.bashrc" ~/.bashrc
 
